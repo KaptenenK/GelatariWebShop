8d4dbbf90bd2e8b42138c168ed460ec96da3a7ae
KaptenenK <koosha.yousefi@hotmail.com>
2023-01-02 15:20:35 +0100

    Service respons tillagd.

--- ECommerceApp/Server/Controllers/ProductController.cs
+++ ECommerceApp/Server/Controllers/ProductController.cs
@@ -14,11 +14,15 @@ namespace ECommerceApp.Server.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProduct()
         {
             var products = await _context.Products.ToListAsync();
+            var response = new ServiceResponse<List<Product>>()
+            {
+                Data = products
+            };
 
-            return Ok(products);
+            return Ok(response);
         }
 
     }
